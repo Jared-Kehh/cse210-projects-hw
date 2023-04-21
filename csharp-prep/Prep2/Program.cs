9a356c26ebefe6a5bb40a286ea66bd090f2691d5
Jared Keh <keh19001@byui.edu>
2023-04-21 13:43:21 -0600

    elif and else statements

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -8,9 +8,26 @@ class Program
         Console.Write("What is your grade? ");
         string grade = Console.ReadLine();
         int percentage = int.Parse(grade);
-        if (percentage < 90)
+        if (percentage >= 90)
         {
             Console.WriteLine("You have an A in your grade");
         }
+        else if (percentage >= 80)
+        {
+            Console.WriteLine("You have a B in your grade");
+        }
+        else if (percentage >= 70)
+        {
+            Console.WriteLine("You have a C in your grade");
+        }
+        else if (percentage >= 60)
+        {
+            Console.WriteLine("You have a D in your grade");
+        }
+        else
+        {
+            Console.WriteLine("You have an F in your grade");
+        }
+
     }
 }
