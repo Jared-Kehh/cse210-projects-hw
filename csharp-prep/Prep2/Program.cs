be5779886a65d80f1063114634163cb426716504
Jared Keh <keh19001@byui.edu>
2023-04-21 10:26:35 -0600

    If statement

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade? ");
+        string grade = Console.ReadLine();
+        int percentage = int.Parse(grade);
+        if (percentage < 90)
+        {
+            Console.WriteLine("You have an A in your grade");
+        }
     }
 }
