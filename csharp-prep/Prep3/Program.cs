1135c5a2bb1912df8cbcb57487ca9b630445498f
Jared Keh <keh19001@byui.edu>
2023-04-24 11:59:46 -0600

    Guessing Game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,41 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        int magicNum = int.Parse(Console.ReadLine());
+        Console.Write("What is your guess? ");
+        int guessNum = int.Parse(Console.ReadLine());
+        if (guessNum > magicNum)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guessNum < magicNum)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Congrats!");
+            }
+        
+        while (magicNum != guessNum)
+        {
+            Console.Write("What is your guess? ");
+            guessNum = int.Parse(Console.ReadLine());
+
+            if (guessNum > magicNum)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guessNum < magicNum)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Congrats!");
+            }
+
+        }
     }
 }
