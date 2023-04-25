bc3f4ef07d1abf403a3f457d6aeab2dc481aa32c
Jared Keh <keh19001@byui.edu>
2023-04-25 09:15:32 -0600

    displaymessage function

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep5 World!");
+        void DisplayMessage()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+        
     }
 }
