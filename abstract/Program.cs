ca24d58873d2588d6c08f1f348a1364fa7754d2f
Jared Keh <keh19001@byui.edu>
2023-04-26 12:22:15 -0600

    Books

---
+++ abstract/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+Book mistborn = new Book("Mistborn", "Brandon Sanderson");
+Book HungerGames = new Book("Hunger Games", "Suzanne Collins");
+
+mistborn.Display();
+HungerGames.Display();
---
+++ abstract/book.cs
@@ -0,0 +1,18 @@
+public class Book
+{
+    private string _author;
+    private string _name;
+    private int _timesRad = 0;
+    private bool _available = true;
+
+    public Book(string name, string author)
+    {
+        _name = name;
+        _author = author;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"{_name} by {_author}");
+    }
+}
