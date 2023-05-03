7799c3a39b5357f7bd99e2592e1ffe93001ed314
Jared Keh <keh19001@byui.edu>
2023-05-03 12:28:00 -0600

    Start with Journal

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -5,5 +5,8 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop02 World!");
+        Console.Write("This is your entry for today: {}");
+        string entry = Console.ReadLine();
+        
     }
 }
