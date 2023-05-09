7074572ef1c8305895b91e743034270dfe58649a
Jared Keh <keh19001@byui.edu>
2023-05-09 08:58:43 -0600

    menu

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("Welcome to your Journal Entry!");
         Display _display = new Display();
         _display.AddEntry(new Prompt("Today I had hash browns!"));
         _display.AddEntry(new Prompt("Yesterday was sunday and I went to church."));
