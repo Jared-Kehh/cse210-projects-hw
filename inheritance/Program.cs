b3348d8ded4ed59d4a7a7fed75558fc339523480
Jared Keh <keh19001@byui.edu>
2023-05-31 19:28:38 -0600

    homework

---
+++ inheritance/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Dvd theWayOfKings = new Dvd("The Way Of Kings");
+Book StarWars = new Book("Star Wars", "795-449950", 50013234);
+StarWars.ShowBookDetails();
+theWayOfKings.CheckOut();
+theWayOfKings.Display();
+
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment Assign1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(Assign1.GetSummary());
+
+        MathAssignment Assign2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(Assign2.GetSummary());
+        Console.WriteLine(Assign2.GetHomeworkList());
+
+        WritingAssignment Assign3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(Assign3.GetSummary());
+        Console.WriteLine(Assign3.GetWritingInformation());
     }
 }
