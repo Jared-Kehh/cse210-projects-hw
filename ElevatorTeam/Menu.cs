df934bf1c05190d17b644a3c37389aaa5023aff9
Jared Keh <keh19001@byui.edu>
2023-06-05 12:16:24 -0600

    prove

--- ElevatorTeam/Menu.cs
+++ ElevatorTeam/Menu.cs
@@ -1,8 +1,7 @@
 public class Menu{
-    string floorNum;
+    public void DisplayMenu(){
+        
+        
 
-    public void DisplayMenu{
-        Console.WriteLine("Pick Elevator Floor From 1-30: ");
-        Console.ReadLine
     }
 }
---
+++ prove/Develop04/Menu.cs
@@ -0,0 +1,25 @@
+public class Menu{
+
+    public void DisplayMenu(){
+
+        string response= "";
+        string[] options= {"1","2","3","4"};
+        while(response!="4"){
+            while(options.Contains(response)==false){
+                Console.WriteLine("Menu Options:");
+                Console.WriteLine("1. Start breathing activity\n2. Start reflecting activity\n3. Start listing activity\n4. Quit");
+                Console.Write("Select a choice from the menu: ");
+                string numChoice = Console.ReadLine() ?? String.Empty;
+
+            }
+            switch(response){
+                case "4":
+                Environment.Exit(0);
+                break;
+                
+            }
+        }
+
+
+    }
+}
