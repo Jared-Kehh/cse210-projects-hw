54af6ce82756aaa001e3e090ca4b7a471209a724
Jared Keh <keh19001@byui.edu>
2023-06-14 12:09:41 -0600

    menu goals

---
+++ prove/Develop05/Menu.cs
@@ -0,0 +1,38 @@
+public class Menu{
+    public void DisplayMenu(){
+        string response = "";
+        string [] options = {"1","2","3","4","5","6"};
+        while(response!="6"){
+            while(options.Contains(response)==false){
+            Console.WriteLine("Menu Options:\n 1. Create New Goal\n 2. List Goals\n 3. Save Goals\n 4. Load Goals\n 5. Record Event\n 6. Quit");
+            Console.Write("Select a choice from the menu: ");
+            response = Console.ReadLine() ?? String.Empty;
+            response = response.ToUpper();
+            
+            }
+
+            switch(response){
+                case "1":
+                break;
+
+                case "2":
+                break;
+
+                case "3":
+                break;
+
+                case "4":
+                break;
+
+                case "5":
+                break;
+
+                case "6":
+                Environment.Exit(0);
+                break;
+            }
+            response="";
+
+        }
+    }
+}
